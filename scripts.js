f1a25a2d6b814309891dd4211184dd4301b092e0
maurolan <mauro.lanzillotta@gmail.com>
2023-08-22 18:55:36 -0300

    correccion menu-toggle display none/flex con JS

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 
+// CAPTURA DE ELEMENTOS
 const menuToggleIcon = document.getElementById('menu-toggle-icon');
 const menuToggle = document.getElementById('menu-toggle');
 
 
+
+
+
+
 // // Click menu hamburguesa --> abre el menutoggle
 menuToggleIcon.addEventListener('click',()=>{
-    menuToggle.style.visibility = 'visible';
+    menuToggle.style.display = 'flex';
 });
 
+// // Click menu hamburguesa --> cierra el menutoggle
 menuToggle.addEventListener('click',()=>{
-menuToggle.style.visibility = 'hidden';
+menuToggle.style.display = 'none';
 });
-
